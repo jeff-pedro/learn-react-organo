71cddc85c0e469c597a40f1759013035a4f2fd12
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-05-13 11:36:37 -0300

    cadastrando colaboradores

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import'./CampoTexto.css';
 
-const CampoTexto = (props) => {
-  const { label, placeholder } = props;
+const CampoTexto = ({ label, placeholder, obrigatorio, valor, aoAlterado }) => {
+
+  const aoDigitado = (evento) => {
+    aoAlterado(evento.target.value);
+  }
+
   return (
     <div className="campo-texto">
       <label>{label}</label>
-      <input type="text" placeholder={placeholder} required={props.obrigatorio}></input>
+      <input value={valor} onChange={aoDigitado} type="text" placeholder={placeholder} required={obrigatorio}></input>
     </div>
   );  
 }
