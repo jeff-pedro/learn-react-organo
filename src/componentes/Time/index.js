bf0c0e6364ca0f6b0885f7d22bc37eb1cc689f8d
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-05-26 18:45:33 -0300

    formata tabulação do documento

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,20 +9,20 @@ const Time = ({ nome, corPrimaria, corSecundaria, colaboradores }) => {
   return (
     (colaboradores.length > 0) && (
       <section className='time' style={style}>
-      <h3>{nome}</h3>
-      <hr style={{ backgroundColor: corPrimaria }} />
-      <div className='colaboradores'>
-        {colaboradores.map(colaborador => 
-          <Colaborador 
-            key={colaborador.nome}
-            nome={colaborador.nome}  
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-            corDeFundo={corPrimaria}
-          />
-        )}
-      </div>
-    </section>)
+        <h3>{nome}</h3>
+        <hr style={{ backgroundColor: corPrimaria }} />
+        <div className='colaboradores'>
+          {colaboradores.map(colaborador => 
+            <Colaborador 
+              key={colaborador.nome}
+              nome={colaborador.nome}  
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+              corDeFundo={corPrimaria}
+            />
+          )}
+        </div>
+      </section>)
   );
 }
 
