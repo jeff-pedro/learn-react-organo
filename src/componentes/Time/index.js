ac65dac580096a5e2b91bd4a47203c7c0547ebf4
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-05-27 10:44:28 -0300

    mudando cor do time (minha implementação)

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import Colaborador from '../Colaborador';
 import './Time.css';
 
 const Time = ({ nome, corPrimaria, corSecundaria, colaboradores, aoDeletarColaborador }) => {
+  const [backgroundColor, setBackgroundColor] = useState(corSecundaria);
+  
   const style = {
-    backgroundColor: corSecundaria,
+    backgroundColor,
   };
+
+  const trocaCorBackround = (e) => {
+    e.preventDefault();
+    setBackgroundColor(e.target.value);
+  }
 
   return (
     (colaboradores.length > 0) && (
       <section className='time' style={style}>
+        <input value={backgroundColor} onChange={trocaCorBackround} type='color' className='input-cor' />
         <h3>{nome}</h3>
         <hr style={{ backgroundColor: corPrimaria }} />
         <div className='colaboradores'>
