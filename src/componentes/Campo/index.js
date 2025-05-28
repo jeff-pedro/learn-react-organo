f472a4a529a3323a715d7fb3e49bcd2fba123e76
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-05-28 13:24:13 -0300

    alterando estilo do campo de cor

--- src/componentes/Campo/index.js
+++ src/componentes/Campo/index.js
@@ -7,9 +7,16 @@ const Campo = ({ type = '', label, placeholder, obrigatorio, valor, aoAlterado }
   }
 
   return (
-    <div className="campo">
+    <div className={`campo campo-${type}`}>
       <label htmlFor={label}>{label}</label>
-      <input type={type} id={label} value={valor} onChange={aoDigitado} placeholder={placeholder} required={obrigatorio}></input>
+      <input 
+        type={type} 
+        id={label} 
+        value={valor} 
+        onChange={aoDigitado} 
+        placeholder={placeholder} 
+        required={obrigatorio}
+      />
     </div>
   );  
 }
